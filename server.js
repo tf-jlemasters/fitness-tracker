f94a4db0b18e5f16ed6fa621634be871bdbfc664
LeMasters <jacksonlemasters@outlook.com>
2021-09-23 16:13:53 -0600

    fixed formatting and added comments

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
+//Required dependencies
 const express = require('express');
 const mongoose = require ('mongoose');
 const logger = require("morgan");
 const path = require('path');
 require('dotenv').config();
-
+//Port
 const PORT= process.env.PORT || 3001;
-//mongoose connection
+
+//Mongoose connection
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -16,16 +18,16 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     .then(() => {
         console.log('Connected to mongodb successfully');
     })
-
     .catch(error => console.log(error));
 
 const app = express();
+
 app.use(logger("dev"));
 app.use(express.static(path.join(__dirname, 'public')));
-//middleware to get values
+//Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+//Routes
 app.use(require('./routes/workoutRoutes.js'));
 app.use(require('./routes/htmlRoutes.js'));
 
